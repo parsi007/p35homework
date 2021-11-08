e738b51a5816c84d1f2918f426f08bfea321cc4e
parsi007 <76106066+parsi007@users.noreply.github.com>
2021-11-08 18:46:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,107 +1,106 @@
- //Create variables here
-var myDatabase,dogImg2,dogImg1,dog,foods,foodStock;
-var feed,addFood;
-var fedTime,lastFeed,foodObj,lastFeed1;
+var database ,dog,dog1,dog2
+var position
+//var form
+var feed,add
+var foodobject
+var Feedtime
+var Lastfeed
+//Create variables here
 
 function preload()
+
 {
-  //load images here 
-  dogImg1 = loadImage("dogImg.png");
-  dogImg2 = loadImage("dogImg1.png"); 
+  dogimg1 = loadImage("images/dogImg.png")
+  dogimg2 = loadImage("images/dogImg1.png")
+  //load images here
+  bedroomImg = loadImage("images/Bed Room.png")
+  gardenImg = loadImage("images/Garden.png")
+  washroomImg = loadImage("images/Wash Room.png")
 }
 
 function setup() {
-  createCanvas(1000,500);
-  myDatabase = firebase.database();
-  foodStock = myDatabase.ref('food'); 
-  foodStock.on("value",readStock);
-  console.log(foodStock,"value");
+	createCanvas(700, 700);
+  database = firebase.database();
+  console.log(database);
+ 
+  foodobject=new Food()
+  dog = createSprite(550,250,10,10);
+  dog.addImage(dogimg1)
+  dog.scale=0.2
+ 
+  var dogo = database.ref('Food');
+  dogo.on("value", readPosition, showError);
+  feed = createButton("FEED DRAGO MILK")
+  feed.position(600,100)
+  feed.mousePressed(FeedDog)
+  add = createButton("ADD FOOD")
+  add.position(400,100)
+  add.mousePressed(AddFood)
+
+
+} 
+
+function draw(){
+ background(46,139,87);
+
+ foodobject.display()
+ 
+ drawSprites();
   
-  
-  dog = createSprite(250,350,50,50);
-  dog.addImage("dog",dogImg1);
-  dog.scale = 0.2;
+ fill(255,255,254);
+ textSize(15);
 
-  foodObj = new food();
-  
-  feed = createButton("Feed the dog");
-  feed.position(700,95);
-  feed.mousePressed(getTime);
-  
-  addFood = createButton("Add food")
-  addFood.position(800,95);
-  addFood.mousePressed(addFoods);
-}
 
-function draw()
- {  
-  background(46,139,87);  
+ fedtime=database.ref('FeedTime')
+ fedtime.on("value",function(data){ Lastfeed=data.val(); });
+ if(Lastfeed>=12)
+ {
+   text("Last Feed :" + Lastfeed%12 + "PM", 150,100);
+ }else if(Lastfeed ===0 )
+ {
+   text("Last Feed : 12 AM" , 150,100)
+ }else
+ {
+   text("Last Feed :" + Lastfeed + "AM", 150,100);
+ }
 
-fedTime = myDatabase.ref("fedTime");
-fedTime.on("value",function(data)
-{
-  lastFeed1 = data.val()
-})
-
-fill(255);
-textSize(20);
-if(lastFeed1 >= 12)
-{
-  text("Last feed: "+ lastFeed1 % 12 + "PM",350,30)
+drawSprites();
 }
-else if(lastFeed1 == 0)
-{
-  text("Last feed : 12 AM",350,30);
-}
-else
-{
-  text("Last feed : " + lastFeed1 + "AM",350,30);
-}
-fill(256);
-text("FEED THE DOGGO AND MAKE HIM HAPPY! :)",400,250);
-   foodObj.display();  
-   //getTime();
-   drawSprites();             
-}
-function readStock(data)
-{
-   foods = data.val(); 
- // console.log("readStock"); 
-   foodObj.updateFoodStock(foods);
- //  console.log(foods,"value");
+function readPosition(data){
+  position = data.val();
+  foodobject.updateFoodStock(position)
 }
 
-function feedDog()
-{
-  dog.addImage(dogImg2);
-  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
-  console.log(lastFeed);
-  myDatabase.ref('/').update({
-    food : foodObj.getFoodStock(),
-    fedTime : lastFeed
+function showError(){
+  console.log("Error in writing to the database");
+}
+
+function writePosition(nazo){
+  if(nazo>0){
+    nazo=nazo-1
+  }
+  else{
+    nazo=0
+  }
+  database.ref('/').set({
+    'Food': nazo
   })
-  //console.log(foodObj);
+
+}
+function AddFood(){
+position++
+database.ref('/').update({
+  Food:position
 }
 
-function addFoods()
-{
-  foods = foods + 1;
-  myDatabase.ref('/').update({
-    food:foods
-  })
- // console.log(foods,"value")
+)
 }
+function FeedDog(){
 
-
-async function getTime()
-{
-  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-  var responseJSON = await response.json();
-
-  var dateTime = responseJSON.datetime;
-   lastFeed = dateTime.slice(11,13);
-
-  text("last Fed : " + lastFeed,230,130);
-  console.log(lastFeed);
-  feedDog();
+dog.addImage(dogimg2)
+foodobject.updateFoodStock(foodobject.getFoodStock()-1)
+ database.ref('/').update({
+   Food:foodobject.getFoodStock(),
+   FeedTime:hour ()
+ })
 }
